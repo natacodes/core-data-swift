4f5ae00bc5cad257076a8cd35b5b12d9805b8271
sinata <natacodes@gmail.com>
2015-03-20 14:25:05 -0500

    Initial Commit

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Nataliia Koldaeva on 3/20/15.
+//  Copyright (c) 2015 Nataliia Koldaeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
